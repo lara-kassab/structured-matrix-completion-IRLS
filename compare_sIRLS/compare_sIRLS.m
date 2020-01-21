89d96ecc627b84db225e22bb4f1e53c32e8e968f
Lara Kassab <larakassab01@gmail.com>
2020-01-21 08:06:46 -0700

    Update compare_sIRLS.m

--- compare_sIRLS/compare_sIRLS.m
+++ compare_sIRLS/compare_sIRLS.m
@@ -17,7 +17,7 @@ p = 1; % Structured sIRLS sparsity parameter
 
 % CHOOSE noise_exp = 0 to run exact recovery experiments
 % CHOOSE noise_exp = 1 to run experiments with noise
-noise_exp = 0;             
+noise_exp = 0;
 eps_noise = 10^(-3); % set the noise parameter (or noise ratio)
 
 % CHOOSE 1 if the Algorithm is allowed to use the information on the rank of the true solution
@@ -56,15 +56,14 @@ r2 = size(rate2_vector,2);
 errorMatA = zeros(r1,r2); % stores errors of sIRLS-q,p
 errorMatB = zeros(r1,r2); % stores errors of Structured sIRLS-q,p
 
-%% ------------- Matrix Completion using both methods ------------- 
+%% ------------- Matrix Completion using both methods -------------
 for k = 1 : numMat
     
     % Construct a random matrix
     YL = sprand(m,r,0.3);
     YR = sprand(r,n,0.5);
     Y = YL*YR; Y = full(Y);
-    Y = Y/norm(Y); 
-    Y_original = Y;
+    Y = Y/norm(Y);
     
     for i = 1 : r1
         rate1 = rate1_vector(i);
@@ -108,13 +107,16 @@ for k = 1 : numMat
             M = [Obs_i, Obs_j, Y(sub2ind(size(Y), Obs_i, Obs_j))];
             
             % Find the error using sIRLS-p
-            errorMatA(i,j) = errorMatA(i,j) + run_sIRLS_q(q,Y_original,M,m,n,r,rknown,2,0);
+            errorMatA(i,j) = errorMatA(i,j) + run_sIRLS_q(q,Y,M,m,n,r,rknown,2,0);
             
             % Find the error using Structured sIRLS-q,p
-            errorMatB(i,j) = errorMatB(i,j) + run_structured_sIRLS(q,p,Y_original,M,m,n,r,rknown, mis_i, mis_j,0);
+            errorMatB(i,j) = errorMatB(i,j) + run_structured_sIRLS(q,p,Y,M,m,n,r,rknown, mis_i, mis_j,0);
+            
+            fprintf('\n\n Working with matrix number %2.1f, \n Sampling rate of nonzero entries = %2.2f, \n Sampling rate of zero entries = %2.2f \n\n', k, rate1, rate2);
             
         end
     end
+    save('errors.mat','errorMatA','errorMatB', 'k')
 end
 
 %% ------------- FIGURES -------------
@@ -129,8 +131,8 @@ xlabel('Sampling rate of zero entries', 'FontSize',18); ylabel('Sampling rate of
 colorbar
 
 % Plot relative errors for each method
-errorMatA = errorMatA./numMat;
-errorMatB = errorMatB./numMat;
+errorMatA = errorMatA./k;
+errorMatB = errorMatB./k;
 
 eA = max(errorMatA(:));
 eB = max(errorMatB(:));
@@ -160,7 +162,7 @@ for i = 1 : r1
     for j = 1 : r2
         if(relError(i,j)>= 1)
             relError_scaled(i,j) = 0;
-        else 
+        else
             relError_scaled(i,j) = 1;
         end
     end
@@ -168,10 +170,10 @@ end
 
 figure;
 imagesc(rate2_vector, rate1_vector, flipud(relError_scaled))
-colormap(gray); 
+colormap(gray);
 set(gca, 'XTick', 0.1:0.1:1, 'XTickLabel', 0.1:0.1:1,'FontSize',14);
 set(gca, 'YTick', 0.1:0.1:1, 'YTickLabel', 1:-0.1:0.1, 'FontSize',14);
-xlabel('Sampling rate of zero entries', 'FontSize',18); 
+xlabel('Sampling rate of zero entries', 'FontSize',18);
 ylabel('Sampling rate of non-zero entries', 'FontSize',18);
 
 %% ------------- END OF FIGURES -------------
